549a2f9f7a84c65e0e8eb6fc67ef8f746a9a1f12
Aung Kyaw Zin <72681459+AlfieKZel@users.noreply.github.com>
2023-12-07 19:23:25 +0800

    Update navigation_behaviors.h

--- Autonomy/include/navigation_behaviors.h
+++ Autonomy/include/navigation_behaviors.h
@@ -5,6 +5,7 @@
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <cstdlib> // For system function
+#include "std_msgs/msg/string.hpp"
 
 class GoToPose : public BT::StatefulActionNode
 {
@@ -15,7 +16,12 @@ public:
 
   using NavigateToPose = nav2_msgs::action::NavigateToPose;
   using GoalHandleNav = rclcpp_action::ClientGoalHandle<NavigateToPose>;
+  
+  // Add a publisher
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr goal_reached_publisher_;
 
+// Add a subscriber
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr goal_reached_subscriber_;
 
   rclcpp::Node::SharedPtr node_ptr_;
   rclcpp_action::Client<NavigateToPose>::SharedPtr action_client_ptr_;
@@ -30,7 +36,8 @@ public:
 
    // Action Client callback
   void nav_to_pose_callback(const GoalHandleNav::WrappedResult &result);
-  /*
+
+  /* Not in use as audio_player_node.cpp not in use 
   // Play audio function
   void playAudio(const std::string &mp3_file_path);
   */
